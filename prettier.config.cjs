c09c4a1b7fcb57d8a329fae2c1e042f520d5b756
Matthew Van Doff <mvandoff@gmail.com>
2025-06-28 08:13:15 -0400

    Set up hero and nav

---
+++ prettier.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+	singleQuote: true,
+	useTabs: true,
+	printWidth: 120,
+	plugins: ['prettier-plugin-astro'],
+	overrides: [
+		{
+			files: '*.astro',
+			options: {
+				parser: 'astro',
+			},
+		},
+	],
+};
